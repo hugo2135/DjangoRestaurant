b26089213ea012684be542920425e70febf2363b
Guo_Ubuntu 22.04 <hugo2135@hotmail.com>
2022-06-02 01:17:30 +0800

    User register constructed

--- RestaurantRecommendSystem/urls.py
+++ RestaurantRecommendSystem/urls.py
@@ -21,5 +21,6 @@ from django.conf.urls import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('restaurants/', include('Restaurant.urls')),
-    path('', include('homeApp.urls'))
+    path('', include('homeApp.urls')),
+    path('accounts/', include('accounts.urls'))
 ]
